f618b355a5647b68e44bb70b5ba792a9ea566c29
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2019-03-22 17:22:56 +0700

    :art: basic usage with env

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -5,8 +5,10 @@ import { StarmanRequestStep } from '../request';
 
 Starman([(runner) => {
   runner("Google should be alive", [
-      new StarmanRequestStep("Status 200").Get("https://www.google.com").AddTest((pm) => {
+      new StarmanRequestStep("Status 200").Get("https://www.{{url}}").AddTest((pm) => {
         pm.response.to.have.status(200)
       })
   ])
-}], {})
+}], {
+  url: "google.com"
+})
