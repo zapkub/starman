830f54c3a184a583ed459a41457760c5fceba215
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2019-03-22 17:31:49 +0700

    :art: Add more test !

---
+++ test/reusing.test.ts
@@ -0,0 +1,17 @@
+import Starman from '..'
+import { StarmanRequestStep } from '../request'
+
+const PingRequest = new StarmanRequestStep('Status 200')
+  .Get('https://www.{{url}}')
+  .AddTest(pm => {
+    pm.response.to.have.status(200)
+  })
+
+// Both Collection use the same request but different env
+Starman([(runner) => runner('Google should be alive', [PingRequest])], {
+  url: 'google.com'
+})
+
+Starman([(runner) => runner('facebook should be alive', [PingRequest])], {
+  url: 'facebook.com'
+})
